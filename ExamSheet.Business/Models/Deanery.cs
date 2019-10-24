a611f3010eca4160fc950f3d81732ef49ca1c530
Arsen <Arsendiabolikkys@users.noreply.github.com>
2019-10-24 16:02:25 +0300

    initial design changes

---
+++ ExamSheet.Business/Models/Deanery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamSheet.Business.Models
+{
+    public class Deanery
+    {
+        public string Id { get; set; }
+
+        //Faculty Faculty {get; set;}
+        //public string MyProperty { get; set; }
+    }
+}
---
+++ ExamSheet.Business/Models/ExamSheet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamSheet.Business.Models
+{
+    public class ExamSheet
+    {
+        public string Id { get; set; }
+
+        public ExamSheetState State { get; set; }
+
+        public DateTime? OpenDate { get; set; }
+
+        public DateTime? CloseDate { get; set; }
+
+        //TODO: XML
+        public string Ratings { get; set; }
+    }
+
+    public enum ExamSheetState
+    {
+        New,
+        Open,
+        Closed,
+        Archived
+    }
+}
